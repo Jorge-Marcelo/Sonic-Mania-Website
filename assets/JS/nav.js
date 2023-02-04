01d209092dedbc8c27959a19beeca608e60b36d0
Jorge-Marcelo <jorge3258@hotmail.com>
2023-02-04 01:10:28 -0300

    Primeiro Commit

---
+++ assets/JS/nav.js
@@ -0,0 +1,9 @@
+/*Abrir o menu*/
+function openNav(){
+document.getElementById("myNav").style.width="100%";
+}
+
+/*Fechar o menu*/
+function closeNav(){
+document.getElementById("myNav").style.width="0%";
+}
