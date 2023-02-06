9a78bef8cab61239548feeced409b937059d3030
Jorge-Marcelo <jorge3258@hotmail.com>
2023-02-05 21:29:25 -0300

    Corrigindo bugs

--- assets/JS/nav.js
+++ assets/JS/nav.js
@@ -1,9 +1,15 @@
-/*Abrir o menu*/
+/*Função para Abrir o menu*/
 function openNav(){
-document.getElementById("myNav").style.width="100%";
+document.getElementById("myNav").style.width="100%"; // Largura do menu 100%
+document.getElementById("myNav").style.display="flex"; // Alterando o diplay do menu de none para flex quando for clicado
+document.getElementById("menu-button-close").style.display="flex";//Mudando o display do botão de fechar
+
+
 }
 
-/*Fechar o menu*/
+/*Função para Fechar o menu*/
 function closeNav(){
-document.getElementById("myNav").style.width="0%";
+document.getElementById("myNav").style.width="0%";//Largura do menu 0 quando for fechado
+document.getElementById("menu-button-close").style.display="none";//Mudando o display do botão de fechar
+
 }
